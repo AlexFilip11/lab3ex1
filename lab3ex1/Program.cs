fef8bdc3b9cf2ca0899d1c5dc7c5aa9f4ae82259
Alex Filip <alexfilippaul@gmail.com>
2022-12-14 15:57:35 +0200

    Add project files.

---
+++ lab3ex1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace lab3ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //suma cifrelor unui numar
+            Console.WriteLine("Introduceti un numar: ");
+            int numar = int.Parse(Console.ReadLine());
+            Console.WriteLine("Suma este " + SumaCifre(numar));
+        }
+
+        static  int SumaCifre(int numar)
+        {
+            int suma=0;
+            while(numar!=0)
+            {
+                suma += numar % 10;
+                numar /= 10;
+            }
+            return suma;
+        }
+    }
+}
